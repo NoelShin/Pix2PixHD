ab9231b857debf8b86bc1f4c30d74121abc5563e
Noel <36836739+NoelShin@users.noreply.github.com>
2018-12-29 18:42:23 +0900

    Update networks.py

--- networks.py
+++ networks.py
@@ -8,94 +8,65 @@ class Generator(nn.Module):
     def __init__(self, opt):
         super(Generator, self).__init__()
         self.opt = opt
-        self.set_weight()
-        self.act = nn.ReLU(inplace=True)
-        self.max_gf = opt.n_gf * 2**opt.n_downsample
-        self.norm = get_norm_layer(opt.norm_type)
-        self.pad = get_pad_layer(opt.padding_type)
+        self.build_model()
         print(self)
 
-    def set_weight(self):
-        self.first_conv = nn.Conv2d(self.opt.input_ch, self.opt.n_gf, kernel_size=7, padding=0)
-        self.last_conv = nn.Conv2d(self.opt.n_gf, self.opt.output_ch, kernel_size=7, padding=0)
-        self.enc_list = nn.ModuleList([nn.Conv2d(self.opt.n_gf * 2 ** i, self.opt.n_gf * 2 ** (i + 1),
-                                                 kernel_size=3, padding=1, stride=2)
-                                       for i in range(self.opt.n_downsample)])
-        self.res_list = nn.ModuleList([nn.Conv2d(self.opt.n_gf * 2 ** self.opt.n_downsample,
-                                                 self.opt.n_gf * 2 ** self.opt.n_downsample,
-                                                 kernel_size=3, padding=0, stride=1)
-                                       for i in range(self.opt.n_residual * 2)])
-        self.dec_list = nn.ModuleList([nn.ConvTranspose2d(self.opt.n_gf * 2 ** (i + 1), self.opt.n_gf * 2 ** i,
-                                                          kernel_size=3, padding=1, stride=2, output_padding=1)
-                                       for i in range(self.opt.n_downsample - 1, -1, -1)])
-
-    def encoder(self, x):
-        model = []
+    def build_model(self):
+        act = nn.ReLU(inplace=True)
         n_gf = self.opt.n_gf
-        model += [self.pad(3), self.first_conv, self.norm(n_gf), self.act]
+        norm = get_norm_layer(self.opt.norm_type)
+        pad = get_pad_layer(self.opt.padding_type)
 
-        for i in range(self.opt.n_downsample):
-            n_gf *= 2
-            model += [self.enc_list[i], self.norm(n_gf), self.act]
-
-        x = nn.Sequential(*model)(x)
-
-        return x
-
-    def residual_block(self, x):
-        for i in range(self.opt.n_residual):
-            y = self.act(self.norm(self.max_gf)(self.res_list[2*i](self.pad(1)(x))))
-            y = self.norm(self.max_gf)(self.res_list[2*i + 1](self.pad(1)(y)))
-            x = x + y
-
-        return x
-
-    def decoder(self, x):
         model = []
-        n_gf = self.opt.n_gf * 2**self.opt.n_downsample
+        model += [pad(3), nn.Conv2d(self.opt.input_ch, n_gf, kernel_size=7, padding=0), act]
 
-        for i in range(self.opt.n_downsample):
-            n_gf /= 2
-            model += [self.dec_list[i], self.norm(int(n_gf)), self.act]
+        for _ in range(self.opt.n_downsample):
+            model += [nn.Conv2d(n_gf, 2 * n_gf, kernel_size=3, padding=1, stride=2), norm(2 * n_gf), act]
+            n_gf *= 2
 
-        model += [self.pad(3), self.last_conv, nn.Tanh()]
+        for _ in range(self.opt.n_residual):
+            model += [ResidualBlock(n_gf, pad, norm, act)]
 
-        x = nn.Sequential(*model)(x)
+        for _ in range(self.opt.n_downsample):
+            model += [nn.ConvTranspose2d(n_gf, n_gf//2, kernel_size=3, padding=1, stride=2, output_padding=1),
+                      norm(n_gf//2), act]
+            n_gf //= 2
 
-        return x
+        model += [pad(3), nn.Conv2d(n_gf, self.opt.output_ch, kernel_size=7, padding=0), nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
 
     def forward(self, x):
-        return self.decoder(self.residual_block(self.encoder(x)))
+        return self.model(x)
 
 
 class PatchDiscriminator(nn.Module):
     def __init__(self, opt):
         super(PatchDiscriminator, self).__init__()
         self.opt = opt
-
-        self.norm = get_norm_layer(opt.norm_type)
-        self.set_weight()
         self.build_model()
 
-    def set_weight(self):
+    def build_model(self):
         input_channel = self.opt.input_ch + self.opt.output_ch
         n_df = self.opt.n_df
-        self.conv_1 = nn.Conv2d(input_channel, n_df, kernel_size=4, padding=1, stride=2)  # 1x64x128x128
-        self.conv_2 = nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2)  # 1x128x64x64
-        self.conv_3 = nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2)  # 1x256x32x32
-        self.conv_4 = nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1)  # 1x512x31x31
-        self.conv_5 = nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1)  # 1x512x30x30
-
-    def build_model(self):
-        n_df = self.opt.n_df
+        norm = get_norm_layer(self.opt.norm_type)
         blocks = []
-        blocks += [[self.conv_1, nn.LeakyReLU(0.2, inplace=True)]]
-        blocks += [[self.conv_2, self.norm(2 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
-        blocks += [[self.conv_3, self.norm(4 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
-        blocks += [[self.conv_4, self.norm(8 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
-        blocks += [[self.conv_5]] if not self.opt.GAN_type == 'GAN' else [[self.conv_5, nn.Sigmoid()]]
-        self.n_blocks = len(blocks)
+        blocks += [[nn.Conv2d(input_channel, n_df, kernel_size=4, padding=1, stride=2),
+                    nn.LeakyReLU(0.2, inplace=True)]]
+        blocks += [[nn.Conv2d(n_df, 2 * n_df, kernel_size=4, padding=1, stride=2),
+                    norm(2 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
+        blocks += [[nn.Conv2d(2 * n_df, 4 * n_df, kernel_size=4, padding=1, stride=2),
+                    norm(4 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
+        blocks += [[nn.Conv2d(4 * n_df, 8 * n_df, kernel_size=4, padding=1, stride=1),
+                    norm(8 * n_df), nn.LeakyReLU(0.2, inplace=True)]]
 
+        if not self.opt.GAN_type == 'GAN':
+            blocks += [[nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1)]]
+
+        else:
+            blocks += [[nn.Conv2d(8 * n_df, 1, kernel_size=4, padding=1, stride=1), nn.Sigmoid()]]
+
+        self.n_blocks = len(blocks)
         for i in range(self.n_blocks):
             setattr(self, 'block_{}'.format(i), nn.Sequential(*blocks[i]))
 
@@ -199,7 +170,7 @@ class Loss(object):
 
         for i in range(self.opt.n_D):
             real_grid = get_grid(real_features[i][-1], is_real=True)
-            fake_grid = get_grid(fake_features[i][-1], is_real=False)  # it doesn't need to be fake_score
+            fake_grid = get_grid(fake_features[i][-1], is_real=False)  # it doesn't need to be fake_features
 
             if self.opt.gpu_ids != -1:
                 real_grid = real_grid.cuda(self.opt.gpu_ids)
@@ -213,6 +184,7 @@ class Loss(object):
         for i in range(self.opt.n_D):
             for j in range(len(fake_features[0])):
                 loss_G_FM += self.FMcriterion(fake_features[i][j], real_features[i][j].detach())
+
             real_grid = get_grid(fake_features[i][-1], is_real=True)
             if self.opt.gpu_ids != -1:
                 real_grid = real_grid.cuda(self.opt.gpu_ids)
@@ -223,11 +195,29 @@ class Loss(object):
         if self.opt.VGG_loss:
             loss_G_VGG_FM = 0
             weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]
-            real_features, fake_features = self.VGGNet(fake), self.VGGNet(target)
+            real_features, fake_features = self.VGGNet(target), self.VGGNet(fake)
 
             for i in range(len(real_features)):
-                loss_G_VGG_FM += weights[i] * self.FMcriterion(real_features[i].detach(), fake_features[i])
-
+                loss_G_VGG_FM += weights[i] * self.FMcriterion(fake_features[i], real_features[i])
             loss_G += loss_G_VGG_FM * self.opt.lambda_FM
 
         return loss_D, loss_G, target, fake
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, n_channels, pad, norm, act):
+        super(ResidualBlock, self).__init__()
+        self.build_block(n_channels, pad, norm, act)
+
+    def build_block(self, n_channels, pad, norm, act):
+        block = [pad(1)]
+        block += [nn.Conv2d(n_channels, n_channels, kernel_size=3, padding=0, stride=1), norm(n_channels), act]
+        block += [pad(1)]
+        block += [nn.Conv2d(n_channels, n_channels, kernel_size=3, padding=0, stride=1), norm(n_channels)]
+
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        x = x + self.block(x)
+
+        return x
