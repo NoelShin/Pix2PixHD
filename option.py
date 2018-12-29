5f5fa40cd2b24f8958b88bb09a47ec7157dfde1e
Noel <36836739+NoelShin@users.noreply.github.com>
2018-12-29 18:43:36 +0900

    Update option.py

--- option.py
+++ option.py
@@ -6,11 +6,11 @@ class BaseOption(object):
     def __init__(self):
         self.parser = argparse.ArgumentParser()
 
-        self.parser.add_argument('--debug', action='store_true', default=True, help='for checking code')
+        self.parser.add_argument('--debug', action='store_true', default=False, help='for checking code')
 
         self.parser.add_argument('--batch_size', type=int, default=1, help='the number of batch_size')
         self.parser.add_argument('--dataset_name', type=str, default='Cityscapes', help='[Cityscapes]')
-        self.parser.add_argument('--gpu_ids', type=int, default=-1, help='gpu number. If -1, use cpu')
+        self.parser.add_argument('--gpu_ids', type=int, default=2, help='gpu number. If -1, use cpu')
         self.parser.add_argument('--image_height', type=int, default=512, help='[512, 1024]')
         self.parser.add_argument('--image_mode', type=str, default='png', help='extension for saving image')
         self.parser.add_argument('--n_downsample', type=int, default=4,
@@ -39,7 +39,7 @@ class TrainOption(BaseOption):
 
         self.parser.add_argument('--beta1', type=float, default=0.5)
         self.parser.add_argument('--beta2', type=float, default=0.999)
-        self.parser.add_argument('--display_freq', type=int, default=10)
+        self.parser.add_argument('--display_freq', type=int, default=500)
         self.parser.add_argument('--epoch_decay', type=int, default=100, help='when to start decay the lr')
         self.parser.add_argument('--eps', type=float, default=1e-8)
         self.parser.add_argument('--FM', action='store_true', default=True, help='switch for feature matching loss')
@@ -47,6 +47,7 @@ class TrainOption(BaseOption):
         self.parser.add_argument('--GAN_type', type=str, default='LSGAN', help='[GAN, LSGAN, WGAN_GP]')
         self.parser.add_argument('--lambda_FM', type=int, default=10, help='weight for FM loss')
         self.parser.add_argument('--lr', type=float, default=0.0002)
+        self.parser.add_argument('--report_freq', type=int, default=5)
         self.parser.add_argument('--save_freq', type=int, default=100000)
         self.parser.add_argument('--shuffle', action='store_true', default=True,
                                  help='if you want to shuffle the order')
