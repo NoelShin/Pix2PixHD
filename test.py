2a604fd6ecde49f8a109e959b2c25c5907f089dd
Noel <36836739+NoelShin@users.noreply.github.com>
2019-02-15 23:19:02 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,39 @@
+import os
+import torch
+import glob
+from pipeline import CustomDataset
+from network import Generator
+from option import TestOption
+from utils.Manger import save_image
+
+import datetime
+
+if __name__ == '__main__':
+    start_time = datetime.datetime.now()
+
+    opt = TestOption().parse()
+    USE_CUDA = True if torch.cuda.is_available() else False
+
+    dataset = CustomDataset(opt)
+    data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=opt.batch_size,
+                                              shuffle=opt.is_train, num_workers=opt.n_workers)
+
+    G = Generator(opt)
+
+    list_G = sorted(glob.glob(os.path.join(opt.model_dir, '*_G.pt')))
+
+    for path_G in list_G:
+        G.load_state_dict(torch.load(path_G))
+
+        if USE_CUDA:
+            G = G.cuda()
+
+        for i, input in enumerate(data_loader):
+            if USE_CUDA:
+                input = input.cuda()
+            fake = G(input)
+            save_image(fake.detach(), os.path.join(opt.image_dir, 'Result_{}'.format(i)))
+            print('{}/{} [{:.{prec}}%] has done.'.format(i + 1, len(data_loader), (i + 1)/len(data_loader)*100,
+                                                         prec=4))
+
+    print("Total time taken: ", datetime.datetime.now() - start_time)
